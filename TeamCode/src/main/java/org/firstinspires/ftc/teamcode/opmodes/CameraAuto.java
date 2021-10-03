a3b4b830cbe51153c7c6cd0f39e25ee05152be6a
Mark <markhaye@yahoo.com>
2021-10-03 13:54:51 -0700

    Added CameraAuto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CameraAuto.java
@@ -0,0 +1,168 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
+import java.util.List;
+
+/*
+adapted from external.samples/ConceptTensorFlowObjectDetection
+it is an example of an autonomous opmode in which the object detection occurs during the init
+phase, before the game is started, meaning the object will have already been detected by the
+time the game starts
+ */
+
+@Autonomous
+
+public class CameraAuto extends LinearOpMode
+{
+    /* Note: This sample uses the all-objects Tensor Flow model (FreightFrenzy_BCDM.tflite), which contains
+     * the following 4 detectable objects
+     *  0: Ball,
+     *  1: Cube,
+     *  2: Duck,
+     *  3: Marker (duck location tape marker)
+     *
+     *  Two additional model assets are available which only contain a subset of the objects:
+     *  FreightFrenzy_BC.tflite  0: Ball,  1: Cube
+     *  FreightFrenzy_DM.tflite  0: Duck,  1: Marker
+     */
+
+    private static final String TFOD_MODEL_ASSET = "FreightFrenzy_BCDM.tflite";
+    private static final String[] LABELS = {
+            "Ball",
+            "Cube",
+            "Duck",
+            "Marker"
+    };
+    private static final String VUFORIA_KEY =
+            "AWfOFc3/////AAABmXMsSsa4mkLNs5JY/osSiR9YHaLw9uPlqefxONDXQPuT7/6lGxp2vfjMuePmvdiXXlKXj+OuYhfiiVTaqEiLUUwZZgMLqKgdkEIYxVHjKU8jo6PzKM/smXAsFUy3AMU+Vn74cleRqpX2y7HqWPYcnwyRGxlYdm2kEGSjtjCvmil5LlpgUtLf2hH0IbMQoXjKwRaANPndk6/s/574vNBkoTbFS9nhdNrTmJ3gW0imfmsGhQIydC+9nIGnAQOqOvTDztn69VVwm+yOUY/eCoqtUngSiAsT6uILXgjpRayDb7QZoOlYSi+NjYtydXZXjskrLeNZ6u4JXix6J8/8ZlrkE611G0JWBEMOjdaQf/oplPhb\n";
+
+    /**
+     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
+     * localization engine.
+     */
+    private VuforiaLocalizer vuforia;
+
+    /**
+     * {@link #tfod} is the variable we will use to store our instance of the TensorFlow Object
+     * Detection engine.
+     */
+    private TFObjectDetector tfod;
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
+        // first.
+        initVuforia();
+        initTfod();
+
+        /**
+         * Activate TensorFlow Object Detection before we wait for the start command.
+         * Do it here so that the Camera Stream window will have the TensorFlow annotations visible.
+         **/
+        if (tfod != null)
+        {
+            tfod.activate();
+
+            // The TensorFlow software will scale the input images from the camera to a lower resolution.
+            // This can result in lower detection accuracy at longer distances (> 55cm or 22").
+            // If your target is at distance greater than 50 cm (20") you can adjust the magnification value
+            // to artificially zoom in to the center of image.  For best results, the "aspectRatio" argument
+            // should be set to the value of the images used to create the TensorFlow Object Detection model
+            // (typically 16/9).
+            tfod.setZoom(2.5, 16.0 / 9.0);
+        }
+
+        // continually detect things during init, until the game starts or is cancelled
+        while (!isStarted() && !isStopRequested() && tfod != null)
+        {
+            // getUpdatedRecognitions() will return null if no new information is available since
+            // the last time that call was made.
+            List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+            if (updatedRecognitions != null)
+            {
+                telemetry.addData("Status", "Initializing");
+                telemetry.addData("# Object Detected", updatedRecognitions.size());
+
+                // step through the list of recognitions and display boundary info.
+                int i = 0;
+                for (Recognition recognition : updatedRecognitions)
+                {
+                    telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                    telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                            recognition.getLeft(), recognition.getTop());
+                    telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                            recognition.getRight(), recognition.getBottom());
+
+                    // figure out where the object is (left, middle, right)
+                    // min and max are values of left side of object in the leftmost and rightmost positions
+                    final double rangeMin = 425.0;
+                    final double rangeMax = 775.0;
+                    final double rangeLen = (rangeMax - rangeMin) / 3;
+                    double testPos = recognition.getLeft();
+                    String position = "middle";
+                    if (testPos > (rangeMax - rangeLen))
+                        position = "right";
+                    else if (testPos < (rangeMin + rangeLen))
+                        position = "left";
+                    telemetry.addData("position", position);
+
+                    i++;
+                }
+                telemetry.update();
+            }
+        }
+
+        // don't need waitforstart, since the above loop already does that
+        // just need to make sure the game wasn't stopped
+        while (opModeIsActive())
+        {
+            telemetry.addData("Status", "Running");
+
+            // do all the autonomous things
+
+            telemetry.update();
+        }
+    }
+
+    /**
+     * Initialize the Vuforia localization engine.
+     */
+    private void initVuforia()
+    {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        // Loading trackables is not necessary for the TensorFlow Object Detection engine.
+    }
+
+    /**
+     * Initialize the TensorFlow Object Detection engine.
+     */
+    private void initTfod()
+    {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minResultConfidence = 0.8f;
+        tfodParameters.isModelTensorFlow2 = true;
+        tfodParameters.inputSize = 320;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABELS);
+    }
+}

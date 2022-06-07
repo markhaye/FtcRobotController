bb30bb3c6e03e7fd8b7ef27f9c7e86fb4e386267
Mark <markhaye@yahoo.com>
2022-06-06 18:31:35 -0700

    Trying to improve turntable initialization accuracy, added servos and potentiometer code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TurretTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TurretTest.java
@@ -2,13 +2,12 @@ package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
-
-import org.firstinspires.ftc.teamcode.hardware.ServoTimed;
 
 @TeleOp
 public class TurretTest extends LinearOpMode
@@ -18,15 +17,18 @@ public class TurretTest extends LinearOpMode
     private static final int TURRET_PLATFORM_GEAR_TEETH = 125;
     private static final double TURRET_COUNT_PER_DEGREE = 1.0 * TURRET_MOTOR_CPR *
             TURRET_PLATFORM_GEAR_TEETH / TURRET_MOTOR_PINION_TEETH / 360.0;
-    private static final double TURRET_MOTOR_INIT_POWER = 0.6;
+    private static final double TURRET_MOTOR_MIN_POWER = 0.2;
+    private static final double TURRET_MOTOR_MAX_POWER = 1.0;
+    private static final double SQRT3 = 1.7320508075688772935274463415059;
 
     private static DcMotor left, right, turretMotor;
+    private static TouchSensor touch, turretLimit;
+    private static Servo servoGrip;                 // REV 41-1097
+    private static CRServo servoArm;               // REV 41-1097
+    private static AnalogInput armRot;
+
     private static double drive, turn, leftPower, rightPower, turretPower, servoPos;
     private static int maxTurretPos, turretPos;
-    private static TouchSensor touch, turretLimit;
-
-    private static Servo servoGrip;                 // REV 41-1097
-    private static CRServo servoSide;               // REV 41-1097
 
     @Override
     public void runOpMode() throws InterruptedException
@@ -39,78 +41,62 @@ public class TurretTest extends LinearOpMode
         turretMotor.setDirection(DcMotor.Direction.FORWARD);
         turretMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         turretMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        turretLimit = hardwareMap.get(TouchSensor.class,"sensorTurretLimit");
-        touch = hardwareMap.get(TouchSensor.class,"sensorTouch");
+        turretLimit = hardwareMap.get(TouchSensor.class, "sensorTurretLimit");
+        touch = hardwareMap.get(TouchSensor.class, "sensorTouch");
+        armRot = hardwareMap.get(AnalogInput.class, "sensorArmRot");
 
-        // following code assumes that the magnetic limit sensor is in between the magnets,
-        // i.e. that the turret is in the legal/safe range
-        // first go towards minimum
-        turretMotor.setPower(-TURRET_MOTOR_INIT_POWER);
-        while (!turretLimit.isPressed() && !isStopRequested())
-        {
-            telemetry.addData("finding", "min turret pos");
-            telemetry.addData("turretLimit", turretLimit.isPressed());
-            telemetry.update();
-        }
-        turretMotor.setPower(0.0);
-        turretMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        turretMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        // min has been set, now have to run forward for at least a little bit to get away from limit
-        turretMotor.setPower(TURRET_MOTOR_INIT_POWER);
-        while((turretPos = turretMotor.getCurrentPosition()) < 100)
-        {
-            telemetry.addData("moving", "away from min turret pos");
-            telemetry.addData("turretLimit", turretLimit.isPressed());
-            telemetry.addData("turretPos", turretPos);
-            telemetry.update();
-        }
-        // now go towards maximum
-        while (!turretLimit.isPressed() && !isStopRequested())
-        {
-            telemetry.addData("finding", "max turret pos");
-            telemetry.addData("turretLimit", turretLimit.isPressed());
-            telemetry.update();
-        }
-        turretMotor.setPower(0.0);
-        maxTurretPos = turretMotor.getCurrentPosition();
-        telemetry.addData("maxTurretPos", maxTurretPos);
-        telemetry.addData("max turret angle", maxTurretPos/TURRET_COUNT_PER_DEGREE);
-        // drive turret to mid position
-        turretMotor.setTargetPosition(maxTurretPos/2);
-        turretMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        turretMotor.setPower(1.0);
-        while (turretMotor.isBusy() && !isStopRequested())
-        {}
-        turretMotor.setPower(0.0);
-        turretMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        /*
-        while (!gamepad1.x)
-        {
-            turretMotor.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
-            telemetry.addData("turretLimit", turretLimit.isPressed());
-            telemetry.addData("set", "min turret pos with X");
-            telemetry.update();
-        }
-        turretMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        turretMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        while (!gamepad1.b)
-        {
-            turretMotor.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
-            telemetry.addData("turretLimit", turretLimit.isPressed());
-            telemetry.addData("set", "max turret pos with B");
-            telemetry.update();
-        }
-        maxTurretPos = turretMotor.getCurrentPosition();
-        telemetry.addData("maxTurretPos", maxTurretPos);
-
-         */
         servoGrip = hardwareMap.get(Servo.class, "servoGrip");
         servoGrip.resetDeviceConfigurationForOpMode();
         servoGrip.setPosition(0.5);
         // continuous rotation servo
-        servoSide = hardwareMap.get(CRServo.class, "servoSide");
-        servoSide.resetDeviceConfigurationForOpMode();
+        servoArm = hardwareMap.get(CRServo.class, "servoArm");
+        servoArm.resetDeviceConfigurationForOpMode();
 
+        // following code assumes that the turret is in the legal/safe range
+        // first go towards minimum
+        telemetry.addData("finding", "min turret pos");
+        telemetry.update();
+        turretMotor.setPower(-TURRET_MOTOR_MAX_POWER);
+        while (!turretLimit.isPressed() && !isStopRequested()) ;
+        turretMotor.setPower(0.0);
+        sleep(100); // give the brakes time to stop the turntable
+        turretMotor.setPower(TURRET_MOTOR_MIN_POWER);
+        while (!turretLimit.isPressed() && !isStopRequested())
+            ; // wait for it to be on again (in case of overshoot)
+        while (turretLimit.isPressed() && !isStopRequested()) ;
+        turretMotor.setPower(0.0);
+        sleep(4096);
+        turretMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        turretMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        telemetry.addData("finding", "max turret pos");
+        telemetry.update();
+        turretMotor.setPower(TURRET_MOTOR_MAX_POWER);
+        // min has been set, now have to run forward for at least a little bit to get away from limit
+        while ((turretMotor.getCurrentPosition() < 40) && !isStopRequested()) ;
+        // now continue towards maximum
+        while (!turretLimit.isPressed() && !isStopRequested()) ;
+        turretMotor.setPower(0.0);
+        sleep(100); // give the brakes time to stop the turntable
+        turretMotor.setPower(-TURRET_MOTOR_MIN_POWER);
+        while (!turretLimit.isPressed() && !isStopRequested())
+            ; // wait for it to be on again (in case of overshoot)
+        while (turretLimit.isPressed() && !isStopRequested()) ;
+        turretMotor.setPower(0.0);
+        maxTurretPos = turretMotor.getCurrentPosition() / 2;
+        sleep(4096);
+        // drive turret to mid position
+        turretMotor.setTargetPosition(maxTurretPos);
+        turretMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        turretMotor.setPower(TURRET_MOTOR_MAX_POWER);
+        while (turretMotor.isBusy() && !isStopRequested()) ;
+        sleep(100); // give it a little more time to settle
+        turretMotor.setPower(0.0);
+        // set midpoint to be 0
+        turretMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        turretMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        telemetry.addData("maxTurretPos", maxTurretPos);
+        telemetry.addData("max turret angle", "%.1f", maxTurretPos / TURRET_COUNT_PER_DEGREE);
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
@@ -127,26 +113,37 @@ public class TurretTest extends LinearOpMode
             // get gamepad input for turret ("a" button overrides turret limits)
             turretPos = turretMotor.getCurrentPosition();
             turretPower = gamepad1.right_trigger - gamepad1.left_trigger;
-            if ((turretPos < 10) && !gamepad1.a) turretPower = Math.max(turretPower, 0.0);
-            if ((turretPos > (maxTurretPos - 10)) && !gamepad1.a) turretPower = Math.min(turretPower, 0.0);
+            if ((turretPos < -maxTurretPos) && !gamepad1.a)
+                turretPower = Math.max(turretPower, 0.0);
+            if ((turretPos > maxTurretPos) && !gamepad1.a)
+                turretPower = Math.min(turretPower, 0.0);
 
             // tell the robot what to do
             left.setPower(leftPower);
             right.setPower(rightPower);
             turretMotor.setPower(turretPower);
 
-            servoPos = 0.5 + gamepad1.right_stick_y * 0.5;
+            servoPos = 0.5 + gamepad1.right_stick_x * 0.5;
             servoGrip.setPosition(servoPos);
-            servoSide.setPower(gamepad1.right_stick_x);
+            servoArm.setPower(gamepad1.right_stick_y);
 
             // display values on the phone
             telemetry.addData("power", "left %.2f right %.2f", leftPower, rightPower);
             telemetry.addData("turretPower", "%.2f", turretPower);
             telemetry.addData("turretPos", turretPos);
-            telemetry.addData("turret angle", "%.1f", turretPos/TURRET_COUNT_PER_DEGREE);
+            telemetry.addData("turret angle", "%.1f", turretPos / TURRET_COUNT_PER_DEGREE);
             telemetry.addData("turretLimit", turretLimit.isPressed());
             telemetry.addData("touch", touch.isPressed());
+            telemetry.addData("armRot", "volts=%.2f, degrees=%.1f", armRot.getVoltage(), GetArmRotDegrees(armRot.getVoltage()));
             telemetry.update();
         }
+    }
+
+    private double GetArmRotDegrees(double voltage)
+    {
+        voltage += 0.0000000001; // make sure it's not zero
+        // formula derived from the following formula from potentiometer's data sheet on REV's website
+        // voltage = 445.5 * (degrees - 270.0) / (degrees * (degrees - 270.0) - 36450.0)
+        return 6.75 * (-Math.sqrt(1089.0 / (voltage * voltage) - (1320.0 / voltage) + 1200.0) + (33.0 / voltage) + 20.0);
     }
 }
